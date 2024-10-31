58f0f7c702a150b1821b2a6c9fc16d34ecf88417
liaminjeong <d.minjeong2@gmail.com>
2024-10-31 20:02:20 +0900

    챕터02 정리본 커밋

---
+++ src/ch09/Main.java
@@ -0,0 +1,11 @@
+package src.ch09;
+
+import java.io.IOException;
+
+public class Main {
+   //
+    public static void main(String[] args) throws IOException {
+        System.out.println("hello");
+    }
+
+}
